c24b0d859825181074545f33cb6c69bd3adea030
VBTX <jv.svedaite@gmail.com>
2019-06-19 17:37:29 -0500

    add function to getStyle for items

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
     {
       id: 2,
       title: "Read",
-      completed: false
+      completed: true
     },
     {
       id: 3,
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,21 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
+	getStyle =() => {
+		return {
+			background: '#f4f4f4',
+			padding: '10px',
+			borderBottom: '1px #ccc dotted',
+			textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+		}
+		}
 render(){
   return (
-  	<div>
-  	<p>{ this.props.todo.title}</p>
+  	<div style= {this.getStyle()}>
+  	<p>
+  	<input type="checkbox" /> {' '}	
+  	{ this.props.todo.title}
+  	</p>
   	</div>
   	)
 }
@@ -15,4 +26,9 @@ TodoItem.propTypes = {
 	todos: PropTypes.object.isRequired
 }
 
+// const itemStyle = {
+// 	backgroundColor: '#f4f4f4'
+// }
+// can be used for style but using function instead because we need
+// more functionality, i.e. if the item is complete it will be crossed out
 export default TodoItem;
