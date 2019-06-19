7fc0881d6a1d48cf494e0dda08bd996fc5289401
VBTX <jv.svedaite@gmail.com>
2019-06-19 18:37:51 -0500

    add todo function with uuid that creates random ids since I dont have a backend here

--- src/App.js
+++ src/App.js
@@ -3,22 +3,23 @@ import './App.css';
 import Header from './components/layout/header'
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 class App extends Component {
 
   state = {
     todos: [
     {
-      id: 1,
+      id: uuid.v4(),
       title: "Learn to code",
       completed: false
     },
     {
-      id: 2,
+      id: uuid.v4(),
       title: "Read",
       completed: false
     },
     {
-      id: 3,
+      id: uuid.v4(),
       title: "Write",
       completed: false
     }
@@ -37,12 +38,21 @@ class App extends Component {
 delTodo = (id) => {
   this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)]})
 }
+
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(),
+    title, 
+    completed: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo]})
+}
   render(){
   return (
     <div className="App">
     <div className="container">
     <Header />
-    <AddTodo />
+    <AddTodo addTodo={this.addTodo}/>
     <h1> Things of importance: </h1>
     <Todos todos={this.state.todos} markComplete={this.markComplete}
     delTodo={this.delTodo}/>
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -5,12 +5,18 @@ export class AddTodo extends Component {
 		title: ''
 	}
 
-	onChange = (e) => this.setState({title: e.target.value});
+	onSubmit = (e) => {
+		e.preventDefault();
+		this.props.addTodo(this.state.title);
+		this.setState({title: ''});
+	}
+
+	onChange = (e) => this.setState({[e.target.name]: e.target.value});
 
 render(){
   return (
-  	<form style={{display:'flex'}}>
-  	<input style={{ flex: '10', padding: '5px'}}type="text" name="title" placeholder="Add Todo...", value={this.state.title} onChange={this.onChange}/>
+  	<form onSubmit={this.onSubmit} style={{display:'flex'}}>
+  	<input style={{ flex: '10', padding: '5px'}}type="text" name="title" placeholder="Add Todo..." value={this.state.title} onChange={this.onChange}/>
   	<input type="submit" value="Submit" className="btn" style={{flex: '1'}}/>
   	</form>
   	)
