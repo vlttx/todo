5715d1a1515a134519a965aa49e45717ed6250bc
VBTX <jv.svedaite@gmail.com>
2019-06-19 16:56:20 -0500

    add components and change function to class

---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Todos extends Component {
+render(){
+  return (
+    <div>
+    <h1>Todos</h1>
+     
+    </div>
+      
+  );
+}
+}
+
+
+export default Todos;
