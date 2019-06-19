dc03c188597b848d61271b95e9848fa31ce097d9
VBTX <jv.svedaite@gmail.com>
2019-06-19 16:58:40 -0500

    add todos to state

--- src/App.js
+++ src/App.js
@@ -3,6 +3,26 @@ import './App.css';
 import Todos from './components/Todos';
 
 class App extends Component {
+
+  state = {
+    todos: [
+    {
+      id: 1;
+      title: "Learn to code";
+      completed: false
+    },
+    {
+      id: 2;
+      title: "Read";
+      completed: false
+    }
+    {
+      id: 3;
+      title: "Write";
+      completed: false
+    }
+    ]
+  }
   render(){
   return (
     <div className="App">
